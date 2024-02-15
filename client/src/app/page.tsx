5e6dc51784fd132ef2bdef04373bc482ca8ee2c1
arya <aryavl9813@gmail.com>
2024-02-15 21:36:29 +0530

    Updated UI

--- client/src/app/page.tsx
+++ client/src/app/page.tsx
@@ -15,21 +15,31 @@ console.log(currentTheme);
   return (
     <>
     
-        
-      <div className={`${currentTheme} h-[100vh] flex items-center justify-center flex-col `}>
-        <h1>Hello</h1>
-        <p>Current theme: {currentTheme} </p>
+        <div className={`${currentTheme} h-[100vh] pt-16`}>
         {currentTheme === "dark" ? (
           <>
+      <div className={`w-[40%] h-[60%]  bg-[#190920]  z-[1000000] relative flex items-center justify-center flex-col mx-auto`}>
+        <h1 className="font-semibold text-lg mb-2">Welcome to Chat Application</h1>
+        <p>Current theme:<span className="font-semibold text-md capitalize"> {currentTheme}</span>  </p>
           <button className="bg-white mt-4 rounded-full py-1 px-6 text-blue-950 font-semibold hover:scale-105" onClick={()=>dispatch(toggleTheme())}>Update theme color</button>
           <Link className="border border-white mt-4 rounded-full py-1 px-10 hover:scale-105" href='/login'>Login</Link>
+      </div>
+        <div className="absolute w-[40%] h-[60%]  z-[10] bg-[#b4cbfc] top-[8rem] right-[22rem]"></div>
           
           </>
 
         ):(
           <>
+         <div className={`w-[40%] h-[60%]  bg-[#190920] text-white  z-[1000000] relative flex items-center justify-center flex-col mx-auto`}>
+          <h1 className="font-semibold text-lg mb-2">Welcome to Chat Application</h1>
+        <p>Current theme:<span className="font-semibold text-md capitalize"> {currentTheme}</span>  </p>
            <button className="bg-blue-950 mt-4 rounded-full py-1 px-6 text-white font-semibold hover:scale-105" onClick={()=>dispatch(toggleTheme())}>Update theme color</button>
         <Link className="border border-blue-950 mt-4 rounded-full py-1 px-10 hover:scale-105" href='/login'>Login</Link>
+      
+      </div>
+      <div className="absolute w-[40%] h-[60%]  z-[10] bg-[#b4cbfc] top-[8rem] right-[22rem]"></div>
+
+
           </>
 
         )}
